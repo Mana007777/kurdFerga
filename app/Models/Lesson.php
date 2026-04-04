d25a5ebcb13ec0f4239448c12d7dc4693884cf45
mana007777 <e7ifubbb@gmail.com>
2026-04-04 12:32:38 +0300

    Lesson model

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Lesson extends Model
 {
-    //
+    protected $fillable = [
+        'course_id',
+        'section_id',
+        'title',
+        'slug',
+        'video_url',
+        'is_preview',
+        'is_published',
+        'sort_order',
+    ];
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function section(): BelongsTo
+    {
+        return $this->belongsTo(Section::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('is_completed')
+            ->withTimestamps();
+    }
 }
