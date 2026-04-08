370cba7cac456d528f93c7ad3c06a878a4d998a3
mana007777 <e7ifubbb@gmail.com>
2026-04-08 21:10:24 +0300

    paths

--- resources/views/pages/settings/⚡profile.blade.php
+++ resources/views/pages/settings/⚡profile.blade.php
@@ -102,7 +102,9 @@ new #[Title('Profile settings')] class extends Component {
                     <div class="relative">
                         <div class="absolute -inset-4 border border-violet-500/20 rounded-full animate-[spin_10s_linear_infinite]"></div>
                         <div class="absolute -inset-2 border border-violet-500/40 rounded-full animate-[spin_15s_linear_infinite_reverse]"></div>
-                        <flux:avatar src="{{ $this->photo ? $this->photo->temporaryUrl() : auth()->user()->profilePhotoUrl() }}" size="xl" class="!w-32 !h-32 !rounded-full shadow-[0_0_30px_rgba(139,92,246,0.3)] relative z-10" />
+                        <div class="w-32 h-32 rounded-full overflow-hidden border-2 border-zinc-800 shadow-[0_0_30px_rgba(139,92,246,0.3)] relative z-10">
+                            <flux:avatar src="{{ $this->photo ? $this->photo->temporaryUrl() : auth()->user()->profilePhotoUrl() }}" class="!w-full !h-full !rounded-none" />
+                        </div>
                     </div>
 
                     <div class="flex-1 space-y-4">
