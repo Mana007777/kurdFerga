7babb06e50cd577c4fabbe54eb4a8cf7fd421f40
mana007777 <e7ifubbb@gmail.com>
2026-04-08 22:08:36 +0300

    paths

--- resources/views/layouts/auth/simple.blade.php
+++ resources/views/layouts/auth/simple.blade.php
@@ -2,6 +2,26 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @include('partials.head')
+        <style>
+            /* Force Flux Input Internals to be Dark */
+            [data-flux-input] input {
+                background-color: transparent !important;
+                color: white !important;
+            }
+            [data-flux-input] {
+                background-color: #09090b !important; /* zinc-950 */
+                border-color: #27272a !important; /* zinc-800 */
+            }
+            /* Handle Browser Autofill */
+            input:-webkit-autofill,
+            input:-webkit-autofill:hover, 
+            input:-webkit-autofill:focus, 
+            input:-webkit-autofill:active {
+                -webkit-box-shadow: 0 0 0 30px #09090b inset !important;
+                -webkit-text-fill-color: white !important;
+                transition: background-color 5000s ease-in-out 0s;
+            }
+        </style>
     </head>
     <body class="min-h-screen bg-zinc-950 text-slate-400 font-sans antialiased overflow-x-hidden relative selection:bg-violet-500 selection:text-white">
 
